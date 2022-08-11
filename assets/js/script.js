c232222bb1ceda40b060d8c6b67ef4e365313974
Tyler Neal <will1neal27@gmail.com>
2022-08-11 01:46:42 -0400

    added the rest of the hours in html added the rest of local storages to save text in boxes also added function to compare planner time to current time for back ground colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
 
-   var time = moment().format('lll'); 
-$('#currentDay').text(time)
+   var currentTime = moment(); 
+$('#currentDay').text(currentTime.format('LL'))
 
 $('.saveBtn').on('click', function(){
-    var key = $(this).parent().attr('id')
+    var key = 'hour-' + $(this).siblings('textarea').attr('id')
     console.log(key)
     var value = $(this).siblings('textarea').val()
     localStorage.setItem(key, value)
 })
 
-$('#hour-9 textarea').val(localStorage.getItem('hour-9')) 
+$('#9').val(localStorage.getItem('hour-9')) 
+$('#10').val(localStorage.getItem('hour-10')) 
+$('#11').val(localStorage.getItem('hour-11')) 
+$('#12').val(localStorage.getItem('hour-12')) 
+$('#13').val(localStorage.getItem('hour-13')) 
+$('#14').val(localStorage.getItem('hour-14')) 
+$('#15').val(localStorage.getItem('hour-15')) 
+$('#16').val(localStorage.getItem('hour-16')) 
+$('#17').val(localStorage.getItem('hour-17')) 
 
+$('.description').each(function(){
+    
+    var hour = Number($(this).attr('id'))
+    
+    var plannerTime = moment().hour(hour).minute(0).second(0);
+    
 
+    if(plannerTime.hour() < currentTime.hour()) {
+        $(this).css('background-color', 'grey')  
+    } else if(plannerTime.hour() === currentTime.hour()) {
+        $(this).css('background-color', 'red')  
+    } else if(plannerTime.hour() > currentTime.hour()) {
+        $(this).css('background-color', 'lightgreen')
+    }
+    
+})
 
 
 
@@ -32,13 +55,13 @@ $('#hour-9 textarea').val(localStorage.getItem('hour-9'))
 // !click listener for save button
     // !set a key and get the value
     // !save to local storage
-// saved data to persist 
-    // from local storage
-    // put this data on the page
-//set past present future colors
-    // get actual time of day
-    // loop thru the set time blocks
-        // get the time of the time block
-        // compare to actual time
-        // based on comparison make past present or future color
+// !saved data to persist 
+    // !from local storage
+    // !put this data on the page
+// !set past present future colors
+    // !get actual time of day
+    // !loop thru the set time blocks
+        // !get the time of the time block
+        // !compare to actual time
+        // !based on comparison make past present or future color
 })
